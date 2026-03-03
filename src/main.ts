7f7da092604a32c124b8f742e47f80d891eae309
Mariano Enzo <marianoenzo00@gmail.com>
2026-03-03 19:07:02 -0300

    cors setup for new domain

--- src/main.ts
+++ src/main.ts
@@ -60,7 +60,10 @@ async function bootstrap() {
   }
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? ['https://urbn.mequiroga.com', 'https://mequiroga.com']
+        : 'http://localhost:5173',
     credentials: true,
   });
 
